b72474878c2929a7ef06c5dc288d8e08da3cb37d
Michael Gilos <michaelg@codev.com>
2017-12-15 09:48:58 +0800

    made index as entry point

--- server/server.js
+++ server/server.js
@@ -2,15 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// Answer API requests.
-app.get('/api', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send('{"message":"Hello from the custom server!"}');
+// Handle API requests.
+app.post('/users', function (req, res) {
+  // res.set('Content-Type', 'application/json');
+  // res.send('{"message":"Hello from the custom server!"}');
+  return res.status(200).end();
 });
 
 // All remaining requests return the React app, so it can handle routing.
@@ -18,6 +18,12 @@ app.get('*', function(request, response) {
   response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-});
+function start() {
+  var port = process.env.PORT || 3001;
+  app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
+  });
+}
+
+exports.start = start;
+exports.app = app;
