0c5faa8fb9ef8d241a0f183b02121df7025f8af8
Michael Gilos <gilosmichael@gmail.com>
2017-12-17 22:11:46 +0800

    fix register page to properly handle /api/users request

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,91 +1,96 @@
 import React, { Component } from 'react';
 
 class Register extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            username: '',
-            password: '',
-            fname: '',
-            lname: '',
-            email: ''
-        };
+    this.state = {
+      username: '',
+      password: '',
+      fname: '',
+      lname: '',
+      email: ''
+    };
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    onChange = (e) => {
-        const state = this.state;
-        state[e.target.name] = e.target.value;
-        this.setState(state);
-    }
+  onChange = (e) => {
+    const state = this.state;
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  }
 
-    handleSubmit(event) {
-        const { username, password, fname, lname, email } = this.state;
-        
-        var param = {
-            user: { username, password, fname, lname, email }
-        };
-        
-        console.log(param.user);
-        
-        fetch('/api/users', {
-              method: 'POST',
-              body: JSON.stringify(param),
-              headers: { "Content-Type": "application/json" }
-            })
-            .then((resp) => resp.json())
-            .then((values) => {
-              console.log(values);
-            });
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const { username, password, fname, lname, email } = this.state;
     
-        event.preventDefault();
-      }
+    var param = {
+      user: { username, password, fname, lname, email }
+    };
+    
+    console.log(param.user);
+    
+    fetch('/api/users', {
+          method: 'POST',
+          body: JSON.stringify(param),
+          headers: { "Content-Type": "application/json" }
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+          console.log(data);
 
-    render() {
-        const { fname, lname, email } = this.state;
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Username:
-                    <input type="text" name="username" onChange={this.onChange} />
-                </label>
+          if (data.error) {
+            alert(data.error);
+          } else {
+            this.props.history.push('/login');
+          }
+        });
+  }
 
-                <br></br>
-                
-                <label>
-                    Password:
-                    <input type="text" name="username" onChange={this.onChange} />
-                </label>
+  render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+            <label>
+                Username:
+                <input type="text" name="username" onChange={this.onChange} />
+            </label>
 
-                <br></br>
+            <br></br>
+            
+            <label>
+                Password:
+                <input type="text" name="password" onChange={this.onChange} />
+            </label>
 
-                <label>
-                    First Name:
-                    <input type="text" name="fname" onChange={this.onChange} />
-                </label>
+            <br></br>
 
-                <br></br>
+            <label>
+                First Name:
+                <input type="text" name="fname" onChange={this.onChange} />
+            </label>
 
-                <label>
-                    Last Name:
-                    <input type="text" name="lname" onChange={this.onChange} />
-                </label>
+            <br></br>
 
-                <br></br>
+            <label>
+                Last Name:
+                <input type="text" name="lname" onChange={this.onChange} />
+            </label>
 
-                <label>
-                    Email:
-                    <input type="text" name="email" onChange={this.onChange} />
-                </label>
+            <br></br>
 
-                <br></br>
-                
-                <input type="submit" value="Create" />
-            </form>
-        );
-    }
+            <label>
+                Email:
+                <input type="text" name="email" onChange={this.onChange} />
+            </label>
+
+            <br></br>
+            
+            <input type="submit" value="Create" />
+        </form>
+    );
+  }
 }
 
 export default Register;
